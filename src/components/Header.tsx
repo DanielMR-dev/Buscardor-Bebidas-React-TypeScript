082ed43627a219c548e211ce15e6d8fb7666de4b
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-10-14 14:41:15 -0500

    321. Leyendo los datos ingresados en el Formulario

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
-import { useEffect, useMemo } from "react";
+import { ChangeEvent, useEffect, useMemo, useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import { useAppStore } from "../stores/useAppStore";
 
 export default function Header() {
+    const [searchFilters, setSearchFilters] = useState({
+        ingredient: '',
+        category: ''
+    });
     const {pathname} = useLocation();
     const isHome = useMemo(() => pathname === '/', [pathname]);
 
@@ -13,6 +17,12 @@ export default function Header() {
         fetchCategories();
     }, []);
 
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        setSearchFilters({
+            ...searchFilters,
+            [e.target.name] : e.target.value
+        })
+    };
     
     return (
         <header className={ isHome ? 'bg-header bg-center bg-cover' : 'bg-slate-800' }>
@@ -54,18 +64,22 @@ export default function Header() {
                                 name="ingredient"
                                 className="p-3 w-full rounded-lg focus:outline-none"
                                 placeholder="Nombre o Ingrediente. Ej Vodka, Tequila, Café"
+                                onChange={handleChange}
+                                value={searchFilters.ingredient}
                             />
                         </div>
                         <div className="space-y-4">
                             <label 
-                                htmlFor="ingredient"
+                                htmlFor="category"
                                 className="nlock text-white uppercase font-extrabold text-lg"
                             >Categoria</label>
 
                             <select
-                                id="ingredient" 
-                                name="ingredient"
+                                id="category" 
+                                name="category"
                                 className="p-3 w-full rounded-lg focus:outline-none"
+                                onChange={handleChange}
+                                value={searchFilters.category}
                             >
                                 <option value="">-- Seleccione --</option>
                                 {categories.drinks.map( category => (
